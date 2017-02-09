37e36d43980f029c43f8173acacce7e185b57ab5
medakk <karanth.karthik@gmail.com>
2017-02-09 20:15:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# lipwig
+lipwig is a server that uses an endpoint to copy data to clipboard. This provides a simple way of sharing text from a mobile device to the PC/laptop. `lipwig` also allows you to view clipboard data from the PC on other devices.
+
+`lipwig` is implemented in Python 3.
+
+### Usage
+
+    lipwig [port number]
+Now, browse to wherever `lipwig` is hosted:
+
+    192.168.1.104:4321/copy
+To view the clipboard, navigate to:
+
+    192.168.1.104:4321/paste
---
+++ lipwig.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+import sys
+
+from subprocess import Popen,PIPE,check_output
+from http.server import BaseHTTPRequestHandler,HTTPServer
+from urllib.parse import urlsplit,parse_qs
+
+DEFAULT_PORT = 4321
+
+def readfile(filepath, binary=True):
+    mode = 'rb' if binary else 'r'
+    with open(filepath, mode) as fd:
+        content = fd.read()
+    return content
+
+HTML_COPYFORM = readfile('./templates/copyform.html')
+HTML_COPYSUCCESS = readfile('./templates/copied.html')
+TEMPLATE_CLIPBOARD = readfile('./templates/clipboard.html', binary=False)
+
+class LipwigHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        split_result = urlsplit(self.path)
+        query = parse_qs(split_result.query)
+
+        if split_result.path in ('/', '/copy'): # HTML form for copying
+            self.send_response(200)
+            mimetype = 'text/html'
+            self.send_header('Content-type', mimetype)
+            self.end_headers()
+            self.wfile.write(HTML_COPYFORM)
+        elif split_result.path=='/do_copy': # GET parameter includes text to copy
+            # Send header
+            self.send_response(200)
+            mimetype = 'text/html'
+            self.send_header('Content-type', mimetype)
+            self.end_headers()
+
+            if 'text' not in query:
+                self.send_error(400, 'No text parameter sent!')
+                return
+            
+            # Copy to clipboard
+            copy_string = query['text'][0]
+            p = Popen(['xsel', '-bi'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            p.communicate(input=bytes(copy_string, 'utf-8'))
+            p.wait()
+
+            # Send response
+            self.wfile.write(HTML_COPYSUCCESS)
+        elif split_result.path=='/clipboard':
+            # Send header
+            self.send_response(200)
+            mimetype = 'text/html'
+            self.send_header('Content-type', mimetype)
+            self.end_headers()
+
+            primary = check_output(['xsel', '-p']).decode('utf-8')
+            clipboard = check_output(['xsel', '-b']).decode('utf-8')
+            response = TEMPLATE_CLIPBOARD.replace('%primary%', primary).replace('%clipboard%', clipboard)
+            self.wfile.write(bytes(response, 'utf-8'))
+        else:
+            self.send_error(404, 'File not found!')
+
+def main():
+    if len(sys.argv)==2:
+        port = int(sys.argv[1])
+    else:
+        port = DEFAULT_PORT
+    try:
+        server = HTTPServer(('', port), LipwigHandler)
+        print('Started server')
+        #print('Started server on {}:{}'.format(*server.client_address))
+
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Ctrl-C pressed. Shutting down server')
+        server.socket.close()
+
+if __name__=='__main__':
+    main()
